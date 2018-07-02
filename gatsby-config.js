173d44523d75aab3ece08e9dda9f959442a12aa5
Gabe Trinidadd <gabe.trinidad@vulcansearch.com>
2018-07-02 11:08:24 -0500

    Blog Commit

---
+++ gatsby-config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  siteMetadata: {
+    title: 'Gatsby Default Starter',
+  },
+  plugins: [
+    'gatsby-plugin-react-helmet', 
+    'gatsby-plugin-netlify-cms',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        path: `${__dirname}/blog`,
+        name: "markdown-pages",
+      },
+    },
+    'gatsby-transformer-remark',
+  ],
+}
